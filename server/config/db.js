93fc64e865cb9b7c48e686c1c3771c771707cc74
Daniela <danielamanzanorangel@email.com>
2024-12-08 23:43:59 -0600

    Minikube files

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,16 @@
 const mongoose = require('mongoose');
-require('dotenv').config({ path: 'variables.env' });
 
 const conectarDB = async () => {
-
     try {
-
-        await mongoose.connect(process.env.DB_MONGO, {
-  
-        })
+        await mongoose.connect(process.env.DB_MONGO, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        });
         console.log('BD Conectada');
-        
     } catch (error) {
         console.log(error);
-        process.exit(1); // Detenemos la app
+        process.exit(1); // Detenemos la app si no se conecta
     }
+};
 
-}
-
-module.exports = conectarDB
+module.exports = conectarDB;
